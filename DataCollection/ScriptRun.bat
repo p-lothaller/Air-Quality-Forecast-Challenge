0f42ecb8bee1fd31ea0e42369e7b15910e0f1fc1
p-lothaller <77859089+p-lothaller@users.noreply.github.com>
2021-03-21 09:49:58 +0200

    Added .bat file for automation process to run script

---
+++ DataCollection/ScriptRun.bat
@@ -0,0 +1,3 @@
+call C:\Users\lotha\anaconda3\Scripts\activate.bat
+call activate data_science
+call python C:\Users\lotha\OneDrive\Documents\ETH\DataScienceProject\Air-Quality-Forecast-Challenge\DataCollection\webscraping.py
