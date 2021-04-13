c7fa95d8e1d43559c708d26bb1c28d477ae7a1da
p-lothaller <77859089+p-lothaller@users.noreply.github.com>
2021-04-13 15:39:26 +0200

    Uploading scrapped data

--- DataCollection/WebScraping.py
+++ DataCollection/WebScraping.py
@@ -11,7 +11,6 @@ from datetime import datetime
 import re
 
 #If a workbook with data already exist, open and save data to it, else create a new workbook to work from
-
 file_name = 'WeatherData.xlsx'
 
 if path.exists(file_name):
@@ -33,7 +32,6 @@ else:
     sheet["K1"] = "AQI Value"
 
 #Adding date, time and location to sheet
-
 today = date.today()
 date = today.strftime("%d/%m/%Y")
 sheet.cell(column=2, row=sheet.max_row+1, value=date)
@@ -44,7 +42,6 @@ location = "stampfenbachstrasse" #Change if the location is changed
 sheet.cell(column=1, row=sheet.max_row, value=location)
 
 #Retrieve information from IQAIR
-
 url = ('https://www.iqair.com/us/switzerland/zurich/zurich-stampfenbachstrasse')
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.set_window_size(480, 320)
@@ -57,17 +54,15 @@ wait = WebDriverWait(driver, 10)
 
 #Retrieve source information of the page
 source = driver.page_source
+
 # quit the driver
 driver.quit()
-
 
 #Creating usable HTML from the source
 soup = BeautifulSoup(source, 'lxml')
 
-
 #Find information regarding the general weather - search for table named "What is the current weather near Zürich Stampfenbachstrasse, Zurich?"
 #This will need to be changed if the location is changed
-
 table_general_weather = soup.find('table', {'title': "What is the current weather near Zürich Stampfenbachstrasse, Zurich?"})
 given_data = ['Weather', 'Temperature', 'Humidity', 'Wind', 'Pressure']
 info = table_general_weather.get_text()
@@ -87,7 +82,6 @@ sheet.cell(column=8, row=sheet.max_row, value=pressure.strip(" mb"))
 
 #Find information regarding the pollutants - search using the class type as there are two tables with the same title from which need to be scraped
 #This will need to be changed if the location is changed
-
 table_detailed_quality = soup.find('table', {'class': "aqi-overview-detail__other-pollution-table"})
 given_data1 = ['PM2.5', 'pm10', 'o3', 'NO2']
 pollutants_detail = table_detailed_quality.findAll('tr')
@@ -96,7 +90,6 @@ for i in pollutants_detail:
     info += str(i.get_text())
 
 #Not all the data is present so we have to see if the data can be collected:
-
 if info.find('PM2.5') != -1:
     pm2_5 = re.search('PM2.5 (.*) ', info).group(1)
     pm2_5Value = pm2_5.split(' ')[1]
